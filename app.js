20f5bc311e83da223e6ade8e2e660b941da1738f
Nitish Aggarwal <royalnitish21@gmail.com>
2019-08-04 11:17:41 +0530

    Set Up BlogSchema and basic app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+var bodyParser = require("body-parser");
+var express = require("express");
+var mongoose = require("mongoose");
+var app = express();
+
+mongoose.connect("mongodb://localhost/restful_blog_app" , { useNewUrlParser: true });
+app.set("view-engine" , "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date , default: Date.now},
+});
+
+var Blog = mongoose.model("Blog" , blogSchema);
+
+//RESTful Routes
+app.get("/" , function(req , res){
+    res.redirect("/blogs");
+})
+
+app.get("/blogs" , function(req , res){
+    Blog.find({} , function(err , blogs){
+        if(err){
+            console.log("Something Went Wrong!!");
+        }else{
+            res.render("index.ejs" , {blogs: blogs});
+        }
+    })
+});
+
+app.listen(3000 , function(){
+    console.log("Blog Server Started at PORT 3000");
+})
