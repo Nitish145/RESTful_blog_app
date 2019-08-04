a1f6c304f110c1d2e410e3972da24e494f6ade70
Nitish Aggarwal <royalnitish21@gmail.com>
2019-08-04 20:08:38 +0530

    bfix: fixed cannnot GET and PUT bugs

--- app.js
+++ app.js
@@ -71,7 +71,6 @@ app.get("/blogs/:id", function (req, res) {
 
 //EDIT ROUTE
 app.get("/blogs/:id/edit", function (req, res) {
-    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.findById(req.params.id, function (err, foundBlog) {
         if (err) {
             res.redirect("/blogs");
